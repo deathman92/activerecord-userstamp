73895fd345db2cee64c739c9de2898c8d5a56c5b
Victor Zhivotikov <deathman1912@gmail.com>
2017-01-14 03:56:54 +0300

    Change column to foreign_key in migration helper

--- lib/active_record/userstamp/migration_additions.rb
+++ lib/active_record/userstamp/migration_additions.rb
@@ -3,10 +3,10 @@ module ActiveRecord::Userstamp::MigrationHelper
 
   def userstamps(*args)
     config = ActiveRecord::Userstamp.config
-    column(config.creator_attribute, :integer, *args)
-    column(config.updater_attribute, :integer, *args)
-    column(config.deleter_attribute, :integer, *args) if config.deleter_attribute.present?
-  end
+    foreign_key(config.default_stamper_class.table_name, *(args << { column: config.creator_attribute }))
+    foreign_key(config.default_stamper_class.table_name, *(args << { column: config.updater_attribute }))
+    foreign_key(config.default_stamper_class.table_name, *(args << { column: config.deleter_attribute })) if config.deleter_attribute.present?
+   end
 end
 
 ActiveRecord::ConnectionAdapters::TableDefinition.class_eval do

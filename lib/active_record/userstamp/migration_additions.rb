077449914b29e7e00187fd0eb30a1493a25ab87f
Victor Zhivotikov <deathman1912@gmail.com>
2017-01-14 04:10:00 +0300

    Fix options merge for foreign keys

--- lib/active_record/userstamp/migration_additions.rb
+++ lib/active_record/userstamp/migration_additions.rb
@@ -3,9 +3,9 @@ module ActiveRecord::Userstamp::MigrationHelper
 
   def userstamps(*args)
     config = ActiveRecord::Userstamp.config
-    foreign_key(config.default_stamper_class.table_name, *(args << { column: config.creator_attribute }))
-    foreign_key(config.default_stamper_class.table_name, *(args << { column: config.updater_attribute }))
-    foreign_key(config.default_stamper_class.table_name, *(args << { column: config.deleter_attribute })) if config.deleter_attribute.present?
+    foreign_key(config.default_stamper_class.table_name, { column: config.creator_attribute }.with_indifferent_access.merge!(args.extract_options!))
+    foreign_key(config.default_stamper_class.table_name, { column: config.updater_attribute }.with_indifferent_access.merge!(args.extract_options!))
+    foreign_key(config.default_stamper_class.table_name, { column: config.deleter_attribute }.with_indifferent_access.merge!(args.extract_options!)) if config.deleter_attribute.present?
    end
 end
 

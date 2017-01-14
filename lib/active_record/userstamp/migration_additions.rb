c7437d19f0a3180b3cea35ac918d8255ab24219d
Victor Zhivotikov <deathman1912@gmail.com>
2017-01-14 14:09:46 +0300

    Get table_name from config

--- lib/active_record/userstamp/migration_additions.rb
+++ lib/active_record/userstamp/migration_additions.rb
@@ -3,9 +3,9 @@ module ActiveRecord::Userstamp::MigrationHelper
 
   def userstamps(*args)
     config = ActiveRecord::Userstamp.config
-    foreign_key(config.default_stamper_class.table_name, { column: config.creator_attribute }.with_indifferent_access.merge!(args.extract_options!))
-    foreign_key(config.default_stamper_class.table_name, { column: config.updater_attribute }.with_indifferent_access.merge!(args.extract_options!))
-    foreign_key(config.default_stamper_class.table_name, { column: config.deleter_attribute }.with_indifferent_access.merge!(args.extract_options!)) if config.deleter_attribute.present?
+    foreign_key(config.default_stamper_table_name, { column: config.creator_attribute }.with_indifferent_access.merge!(args.extract_options!))
+    foreign_key(config.default_stamper_table_name, { column: config.updater_attribute }.with_indifferent_access.merge!(args.extract_options!))
+    foreign_key(config.default_stamper_table_name, { column: config.deleter_attribute }.with_indifferent_access.merge!(args.extract_options!)) if config.deleter_attribute.present?
    end
 end
 
